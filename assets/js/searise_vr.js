bafc41ce62a94e0ec4dddea3438a79da12e4ede3
Rensa <me@rensa.co>
2017-02-21 23:03:29 +1100

    Searise: animation test fix

--- assets/js/searise_vr.js
+++ assets/js/searise_vr.js
@@ -1,10 +1,1 @@
 /* some javascript for my searise test vr. mostly event handlers (tho isn't all javascript?). james goldie (rensa.co), feb 2017 */
-
-AFRAME.registerComponent('coogee-text', {
-  
-  // emit text events when the scene is initialised
-  init: function () {
-    console.log('Initialising text element')
-    this.el.emit('start-tour');
-  }
-});
