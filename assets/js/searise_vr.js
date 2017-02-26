a3129c3c2aa93a5007fb1cad4a56a109a41e4ba5
Rensa <me@rensa.co>
2017-02-26 17:42:02 +1100

    (Broken)

--- assets/js/searise_vr.js
+++ assets/js/searise_vr.js
@@ -1,47 +1,46 @@
 /* some javascript for my searise test vr. mostly event handlers (tho isn't all javascript?). james goldie (rensa.co), feb 2017 */
 
-// start_coogee2006:
-//     - begin scene animations (inc. fading out start btn)
-//     - disable start btn click
-function start_coogee2006()
-{
-    console.log('Starting scene 1a (Coogee, 2006)');
-    var sky = document.querySelector("#coogee2006-sky");
-    var text = document.querySelector("#coogee2006-text");
-    var start_btn = document.querySelector("#start-btn");
-
-    // make sky and text visible before fading in
-    sky.setAttribute('visible', 'true');
-    text.setAttribute('visible', 'true');
-
-    // start fading them in
-    text.emit("coogeetour");
-    sky.emit("coogeetour");
-
-    // start fading button out and disable clicking it again
-    start_btn.emit("coogeetour");
-    start_btn.removeEventListener('click', start_coogee2006);
-}
-
 // register click event for start button
-AFRAME.registerComponent('start-btn',
+AFRAME.registerComponent('btn-coogee',
 {
     init: function()
     {
-        this.el.addEventListener('click', start_coogee2006);
+        console.log('Coogee button component registered');
+        this.el.addEventListener('click', btn_coogee_fadeout);
     }
 });
 
-// start-btn-fadeout
-AFRAME.registerComponent('start-btn-fadeout',
+// ani-btn-coogee: when it ends, start coogee scene up
+AFRAME.registerComponent('ani-btn-coogee',
 {
     init: function()
     {
-        // stop rendering button once it fades out
+        console.log('Coogee button animation component registered');
+        // end fade out: turn off, start scene fade in
         this.el.addEventListener('animationend', function()
         {
-            document.querySelector("#start-btn").
-                setAttribute('visible', 'false');
+            var sky = document.querySelector("#coogee2006-sky");
+            var text = document.querySelector("#coogee2006-text");
+            var start_btn = document.querySelector("#btn-coogee");
+            
+            console.log('Starting scene 1a (Coogee, 2006)');
+            start_btn.setAttribute('visible', 'false');
+
+            // make sky and text visible before fading in
+            sky.setAttribute('visible', 'true');
+            text.setAttribute('visible', 'true');
+            text.emit("fadein");
+            sky.emit("fadein");
         });
     }
 });
+
+// btn_coogee_fadeout: emit event for btn fadeout ani, turn off click listener
+function btn_coogee_fadeout()
+{
+    console.log('Coogee button fading');
+    var start_btn = document.querySelector("#btn-coogee");
+    
+    start_btn.emit("fadeout");
+    start_btn.removeEventListener('click', btn_coogee_fadeout);
+}
