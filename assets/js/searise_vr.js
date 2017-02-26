1df19faa8c3052cb43f7d616f25d3a8a4bac5f5f
Rensa <me@rensa.co>
2017-02-26 18:11:58 +1100

    Searise: now using different (better!) animation component

--- assets/js/searise_vr.js
+++ assets/js/searise_vr.js
@@ -1,46 +1,50 @@
 /* some javascript for my searise test vr. mostly event handlers (tho isn't all javascript?). james goldie (rensa.co), feb 2017 */
 
-// register click event for start button
+// register event handlers for start button
 AFRAME.registerComponent('btn-coogee',
 {
     init: function()
     {
         console.log('Coogee button component registered');
         this.el.addEventListener('click', btn_coogee_fadeout);
+        this.el.addEventListener('animationbegin', btn_coogee_disable)
+        this.el.addEventListener('animationcomplete', coogee2006_scene_start)
     }
 });
 
-// ani-btn-coogee: when it ends, start coogee scene up
-AFRAME.registerComponent('ani-btn-coogee',
-{
-    init: function()
-    {
-        console.log('Coogee button animation component registered');
-        // end fade out: turn off, start scene fade in
-        this.el.addEventListener('animationend', function()
-        {
-            var sky = document.querySelector("#coogee2006-sky");
-            var text = document.querySelector("#coogee2006-text");
-            var start_btn = document.querySelector("#btn-coogee");
-            
-            console.log('Starting scene 1a (Coogee, 2006)');
-            start_btn.setAttribute('visible', 'false');
-
-            // make sky and text visible before fading in
-            sky.setAttribute('visible', 'true');
-            text.setAttribute('visible', 'true');
-            text.emit("fadein");
-            sky.emit("fadein");
-        });
-    }
-});
+/* event handler definitions ------------------------------------------------ */
 
 // btn_coogee_fadeout: emit event for btn fadeout ani, turn off click listener
 function btn_coogee_fadeout()
 {
     console.log('Coogee button fading');
+    document.querySelector("#btn-coogee").
+        emit("fadeout");
+}
+
+function btn_coogee_disable()
+{
+    console.log('Coogee button disabled');
+    document.querySelector("#btn-coogee").
+        removeEventListener('click', btn_coogee_fadeout);
+}
+
+function coogee2006_scene_start()
+{
+    console.log('Starting scene 1a (Coogee, 2006)');
+    var sky = document.querySelector("#coogee2006-sky");
+    var text = document.querySelector("#coogee2006-text");
     var start_btn = document.querySelector("#btn-coogee");
     
-    start_btn.emit("fadeout");
-    start_btn.removeEventListener('click', btn_coogee_fadeout);
+    // stop rendering start button
+    start_btn.setAttribute('visible', 'false');
+
+    // make sky and text visible before fading in
+    sky.setAttribute('visible', 'true');
+    text.setAttribute('visible', 'true');
+    sky.emit("fadein");
+    setTimeout(function() { text.emit("fadein"); }, 2000)
+
+    // nb: should be able to specify delay: 4000 in component, but it's
+    // not working with startEvents for some reason :/
 }
